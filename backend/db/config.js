c764c014c2b804566a920b9ab493641e7fb63a17
saidul-anam <saidulanamsiam2003@gmail.com>
2024-10-14 22:29:51 +0600

    commmit

--- backend/db/config.js
+++ backend/db/config.js
@@ -1,4 +1,6 @@
-const mongoose=require('mongoose');
-mongoose.connect("mongodb://localhost:27017/e-com",{
-    family: 4,
-});  
+const mongoose = require('mongoose');
+require('dotenv').config();
+const uri = process.env.MONGODB_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("MongoDB connected"))
+    .catch(err => console.error("MongoDB connection error:", err));
